49db81f501caf979e1322fae0d6a526d2e782dd0
AmbroseL <ambrose2015@icloud.com>
2019-10-12 14:25:43 -0400

    List opacity

--- script/case.js
+++ script/case.js
@@ -19,9 +19,7 @@ $(function(){
         once: false, // whether animation should happen only once - while scrolling down
         mirror: false, // whether elements should animate out while scrolling past them
         anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-      
       });
-      $(".content-list").css('opacity','0');
 
     // init Masonry
     var $grid = $('.grid').masonry({
